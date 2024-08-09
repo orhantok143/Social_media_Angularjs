76841a64111f538b00e5c072aebcbe8397839f1a
orhan <orhantok143@gmail.com>
2024-08-09 22:13:26 +0300

    v1.0.0

---
+++ src/app/components/chattype/chattype.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-chattype',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './chattype.component.html',
+  styleUrl: './chattype.component.css'
+})
+export class ChattypeComponent {
+
+}
